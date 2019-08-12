bd23dfeea52acb02522bd3221566324ebb3346ae
lurongshuang <13269779672@163.com>
2019-08-12 12:10:12 +0800

    实现 查看链接PDF 模拟在线查看（下载到本地在打开）

--- app/src/main/java/com/pdf_examplebyl/MainActivity.java
+++ app/src/main/java/com/pdf_examplebyl/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.artifex.mupdfdemo.MuPDFActivity;
+import com.artifex.mupdfdemo.NetWorkPDF.NetDowActivity;
 
 import java.io.File;
 
@@ -26,10 +27,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                File file = new File(Environment.getExternalStorageDirectory().getPath() + "/SL 27-2014 水闸施工规范.pdf");
-                if (file.exists()) {
-                    openFile("pdf", file + "");
-                }
+//                File file = new File(Environment.getExternalStorageDirectory().getPath() + "/SL 27-2014 水闸施工规范.pdf");
+//                if (file.exists()) {
+//                    openFile("pdf", file + "");
+//                }
+                Intent intent = new Intent(MainActivity.this, NetDowActivity.class);
+                String url ="https://file3.data.weipan.cn/1231300/9c7a47a4d973f86702f41a608d92bdbd3d69eb3c?ip=1565583179,120.244.92.201&ssig=NLzhomRfUn&Expires=1565583779&KID=sae,l30zoo1wmz&fn=%E7%AE%80%E5%8E%86%E4%B8%8E%E6%B1%82%E8%81%8C%E4%BF%A12015%E6%A0%A1%E5%9B%AD%E6%8B%9B%E8%81%98%E6%B1%82%E8%81%8C%E5%A4%A7%E7%A4%BC%E5%8C%85.pdf&se_ip_debug=120.244.92.201&from=1221134";
+                intent.putExtra("path",url);
+                intent.putExtra("LoPath",getApplicationContext().getExternalFilesDir(null).toString());
+                startActivity(intent);
 
             }
         });
---
+++ pdflibrarybyl/src/main/java/com/artifex/mupdfdemo/NetWorkPDF/NetDowActivity.java
@@ -0,0 +1,196 @@
+package com.artifex.mupdfdemo.NetWorkPDF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.artifex.mupdfdemo.MuPDFActivity;
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadListener;
+import com.liulishuo.filedownloader.FileDownloader;
+import com.lrs.pdflibrarybyl.R;
+
+import java.io.File;
+
+public class NetDowActivity extends Activity {
+    //请求地址
+    private String path;
+    //文件名称
+    private String fileName;
+    //本机地址
+    private static String LoPath = "";
+    //缓存目录
+    private static String cachePath = "";
+    //PDF文件 保存地址
+    private static String PDFPath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_dow);
+        Intent intent = getIntent();
+        LoPath = intent.getStringExtra("LoPath");
+        cachePath = LoPath + "/.DwnloadFile";
+        PDFPath = cachePath + "/.PDF";
+        path = intent.getStringExtra("path");
+        FileDownloader.setup(this);   //ac为activity的上下文对象
+        initData();
+    }
+
+    /*
+        初始化
+     */
+    private void initData() {
+        //初始化 文件目录
+        File pdffile = new File(PDFPath);
+        if (!pdffile.exists()) {
+            pdffile.mkdirs();
+        }
+        //获取文件名称
+        fileName = getFileName(path);
+        if (!getExistsOfPath(PDFPath, fileName)) {
+            //下载文件
+            dowFile(path, PDFPath + "/" + fileName);
+        } else {
+            File file = new File(PDFPath + "/" + fileName);
+            if (file.exists()) {
+                openFile(file + "");
+            }
+        }
+
+    }
+
+    public static String getFileName(String Path) {
+        String fileName = null;
+        if (Path != null && Path.length() > 0) {
+            fileName = Path.substring(Path.lastIndexOf("/") + 1);
+        }
+        return fileName;
+//        return "test1.pdf";
+    }
+
+    public static boolean getIsExists(String path) {
+        boolean isexists = false;
+        String fileName = getFileName(path);
+        if (fileName != null && fileName.length() > 0) {
+            isexists = getExistsOfPath(PDFPath, path);
+        }
+        return isexists;
+    }
+
+    /*
+        指定文件夹是否存在指定文件
+     */
+    public static boolean getExistsOfPath(String PDFPath, String name) {
+        boolean isex = false;
+        File file = new File(PDFPath);
+        File[] files = file.listFiles();
+        if (files != null && file.length() > 0) {
+            for (int i = 0; i < files.length; i++) {
+                if (!files[i].isDirectory()) {
+                    String fileName = files[i].getName();
+                    if (fileName != null && fileName.length() > 0 && fileName.equals(name)) {
+                        isex = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return isex;
+    }
+
+    public void openFile(String filePath) {
+        Uri uri = Uri.parse(filePath);
+        Intent intent = new Intent(this, MuPDFActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(uri);
+        startActivity(intent);
+        this.finish();
+
+    }
+
+
+    private void dowFile(String url, final String fileUrl) {
+        FileDownloader.getImpl().create(url)
+                .setPath(fileUrl)
+                .setForceReDownload(true)
+                .setListener(new FileDownloadListener() {
+                    //等待
+                    @Override
+                    protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                        showProgressDialog(NetDowActivity.this, "加载中");
+                    }
+
+                    //下载进度回调
+                    @Override
+                    protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                    }
+                    //完成下载
+                    @Override
+                    protected void completed(BaseDownloadTask task) {
+                        dismissProgressDialog();
+                        openFile(fileUrl);
+                    }
+                    //暂停
+                    @Override
+                    protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+
+                    }
+
+                    //下载出错
+                    @Override
+                    protected void error(BaseDownloadTask task, Throwable e) {
+                        Toast.makeText(NetDowActivity.this, "加载失败,请重试", Toast.LENGTH_SHORT).show();
+                        dismissProgressDialog();
+                        NetDowActivity.this.finish();
+                    }
+
+                    //已存在相同下载
+                    @Override
+                    protected void warn(BaseDownloadTask task) {
+                        openFile(fileUrl);
+                    }
+                }).start();
+    }
+
+    //加载框变量
+    private ProgressDialog progressDialog;
+
+    public void showProgressDialog(Context mContext, String text) {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(mContext);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        }
+        progressDialog.setMessage(text);    //设置内容
+        progressDialog.setCancelable(false);//点击屏幕和按返回键都不能取消加载框
+        progressDialog.show();
+
+        //设置超时自动消失
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //取消加载框
+                if (dismissProgressDialog()) {
+                    //超时处理
+                }
+            }
+        }, 60000);//超时时间60秒
+    }
+
+    public Boolean dismissProgressDialog() {
+        if (progressDialog != null) {
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+                return true;//取消成功
+            }
+        }
+        return false;
+    }
+}
